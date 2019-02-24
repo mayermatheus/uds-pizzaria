eb514deaef159bb2e4bb8ce48cbf5324011b69fb
Matheus Mayer <matheusmayerduarte@gmail.com>
2019-02-24 15:29:17 -0300

    Resolve #3 - adiciona mapeamento da entidade

---
+++ src/main/java/br/com/uds/udspizzaria/domain/model/tamanho/Tamanho.java
@@ -0,0 +1,63 @@
+package br.com.uds.udspizzaria.domain.model.tamanho;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tamanho")
+public class Tamanho implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "nome")
+	private String nome;
+	
+	@Column(name = "valor")
+	private BigDecimal valor;
+	
+	@Column(name = "tempo_preparo")
+	private Integer tempoPreparo;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public BigDecimal getValor() {
+		return valor;
+	}
+
+	public void setValor(BigDecimal valor) {
+		this.valor = valor;
+	}
+
+	public Integer getTempoPreparo() {
+		return tempoPreparo;
+	}
+
+	public void setTempoPreparo(Integer tempoPreparo) {
+		this.tempoPreparo = tempoPreparo;
+	}
+
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+}
