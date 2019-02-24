72d371bdd12cb33e7abac7a6c83750b2653bf65d
Matheus Mayer <matheusmayerduarte@gmail.com>
2019-02-24 20:51:36 -0300

    Resolve #16 - Cria repositório adicional

---
+++ src/main/java/br/com/uds/udspizzaria/infrastructure/persistence/hibernate/repository/AdicionalRepositoryInterface.java
@@ -0,0 +1,9 @@
+package br.com.uds.udspizzaria.infrastructure.persistence.hibernate.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.uds.udspizzaria.domain.model.adicional.Adicional;
+
+public interface AdicionalRepositoryInterface extends JpaRepository<Adicional, Long>{
+
+}
