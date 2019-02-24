c292355d274cd088c8d144b008829a72cc747d03
Matheus Mayer <matheusmayerduarte@gmail.com>
2019-02-23 21:43:45 -0300

    Init commit

---
+++ src/main/java/br/com/uds/udspizzaria/UdsPizzariaApplication.java
@@ -0,0 +1,13 @@
+package br.com.uds.udspizzaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdsPizzariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdsPizzariaApplication.class, args);
+	}
+
+}
