d6cd5daab1314961d67836c784d113bb6a82bcac
Matheus Mayer <matheusmayerduarte@gmail.com>
2019-02-24 15:33:05 -0300

    Resolve #1 - Cria a estrutura inicial com a abordagem DDD

---
+++ src/main/java/br/com/uds/udspizzaria/application/service/exception/InformacaoNaoEncontradaException.java
@@ -0,0 +1,7 @@
+package br.com.uds.udspizzaria.application.service.exception;
+
+public class InformacaoNaoEncontradaException extends RuntimeException  {
+
+	private static final long serialVersionUID = 1L;
+
+}
---
+++ src/main/java/br/com/uds/udspizzaria/domain/service/BaseServiceInterface.java
@@ -0,0 +1,15 @@
+package br.com.uds.udspizzaria.domain.service;
+
+import java.util.List;
+
+public interface BaseServiceInterface<T> {
+    public T buscar(Long id);
+
+    public List<T> listarTodos();
+
+    public T salvar(T entidade);
+
+    public T atualizar(Long id, T entidade);
+
+    public void remover(Long id);
+}
