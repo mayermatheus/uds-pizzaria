961ed784097c11e1e1a96ec7e74cd5e409fdb42d
Matheus Mayer <matheusmayerduarte@gmail.com>
2019-02-24 16:30:58 -0300

    Resolve #10 - Cria interface do dominio de sabor

---
+++ src/main/java/br/com/uds/udspizzaria/domain/service/SaborServiceInterface.java
@@ -0,0 +1,7 @@
+package br.com.uds.udspizzaria.domain.service;
+
+import br.com.uds.udspizzaria.domain.model.sabor.Sabor;
+
+public interface SaborServiceInterface extends BaseServiceInterface<Sabor>{
+
+}
