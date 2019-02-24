f5166d710a90a898149248337045811e37b3bee9
Matheus Mayer <matheusmayerduarte@gmail.com>
2019-02-24 15:30:32 -0300

    Resolve #2 - Cria controller para retornar todos os tamanhos das pizzas

---
+++ src/main/java/br/com/uds/udspizzaria/application/controller/TamanhoController.java
@@ -0,0 +1,24 @@
+package br.com.uds.udspizzaria.application.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.uds.udspizzaria.application.service.TamanhoService;
+import br.com.uds.udspizzaria.presentation.dto.TamanhoDTO;
+
+@RestController
+@RequestMapping("/tamanhos")
+public class TamanhoController {
+
+	@Autowired
+	protected TamanhoService tamanhoService;
+	
+	@GetMapping
+	public List<TamanhoDTO> listar() {		
+		return tamanhoService.listarTodosComIdENome();
+	}
+}
