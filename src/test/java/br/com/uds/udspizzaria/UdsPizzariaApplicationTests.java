f27334cbd5abc621cfca3c968a21f78772caf2ba
Matheus Mayer <matheusmayerduarte@gmail.com>
2019-02-27 10:28:47 -0300

    Adicionando para os testes apontarem para o application-test

--- src/test/java/br/com/uds/udspizzaria/UdsPizzariaApplicationTests.java
+++ src/test/java/br/com/uds/udspizzaria/UdsPizzariaApplicationTests.java
@@ -2,13 +2,14 @@ package br.com.uds.udspizzaria;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.ConfigFileApplicationContextInitializer;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@TestPropertySource(locations="classpath:application-test.properties")
+@ContextConfiguration(initializers = ConfigFileApplicationContextInitializer.class) 
 public class UdsPizzariaApplicationTests {
 
 	@Test
