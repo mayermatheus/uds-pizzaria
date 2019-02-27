3d2f25ee72336c068a6c5de2fea21d4a7d05b83d
Matheus Mayer <matheusmayerduarte@gmail.com>
2019-02-27 10:57:56 -0300

    Ajuste no testes unitarios

--- src/test/java/br/com/uds/udspizzaria/UdsPizzariaApplicationTests.java
+++ src/test/java/br/com/uds/udspizzaria/UdsPizzariaApplicationTests.java
@@ -2,14 +2,13 @@ package br.com.uds.udspizzaria;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.ConfigFileApplicationContextInitializer;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@ContextConfiguration(initializers = ConfigFileApplicationContextInitializer.class) 
+@TestPropertySource 
 public class UdsPizzariaApplicationTests {
 
 	@Test
--- src/test/java/br/com/uds/udspizzaria/application/service/PedidoServiceTest.java
+++ src/test/java/br/com/uds/udspizzaria/application/service/PedidoServiceTest.java
@@ -10,7 +10,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import br.com.uds.udspizzaria.domain.model.adicional.Adicional;
@@ -23,7 +23,7 @@ import br.com.uds.udspizzaria.presentation.dto.PedidoDTO;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@ActiveProfiles("test")
+@TestPropertySource
 public class PedidoServiceTest {
 
 	@Autowired
