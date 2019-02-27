ec2bd3ed75d6ae968dd91d693c2683bc552c3acd
Matheus Mayer <matheusmayerduarte@gmail.com>
2019-02-26 23:58:36 -0300

    Adicionando novas excecoes

---
+++ src/main/java/br/com/uds/udspizzaria/application/service/exception/AdicionalNaoEncontradoException.java
@@ -0,0 +1,5 @@
+package br.com.uds.udspizzaria.application.service.exception;
+
+public class AdicionalNaoEncontradoException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+}
