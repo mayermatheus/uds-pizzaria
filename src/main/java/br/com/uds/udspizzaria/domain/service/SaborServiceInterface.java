5209357ea1c6a40d50a8f0f487b560dabe68aef4
Matheus Mayer <matheusmayerduarte@gmail.com>
2019-02-26 21:58:41 -0300

    Adicionando metodo para encontrar o nome e o id de todos os sabores

--- src/main/java/br/com/uds/udspizzaria/domain/service/SaborServiceInterface.java
+++ src/main/java/br/com/uds/udspizzaria/domain/service/SaborServiceInterface.java
@@ -1,7 +1,10 @@
 package br.com.uds.udspizzaria.domain.service;
 
+import java.util.List;
+
 import br.com.uds.udspizzaria.domain.model.sabor.Sabor;
+import br.com.uds.udspizzaria.presentation.dto.SaborDTO;
 
-public interface SaborServiceInterface extends BaseServiceInterface<Sabor>{
-
+public interface SaborServiceInterface extends BaseServiceInterface<Sabor> {
+	public List<SaborDTO> listarTodosComIdENome();
 }
