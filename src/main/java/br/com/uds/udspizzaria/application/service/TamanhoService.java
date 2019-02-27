e2c419126f56371ad9bf5634ea0f044d7d21451a
Matheus Mayer <matheusmayerduarte@gmail.com>
2019-02-27 00:13:15 -0300

    Sobrescrevendo metodo para sobrescrever a validacao correta

--- src/main/java/br/com/uds/udspizzaria/application/service/TamanhoService.java
+++ src/main/java/br/com/uds/udspizzaria/application/service/TamanhoService.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.uds.udspizzaria.application.service.exception.InformacaoNaoEncontradaException;
+import br.com.uds.udspizzaria.application.service.exception.TamanhoNaoEncontradoException;
 import br.com.uds.udspizzaria.domain.model.tamanho.Tamanho;
 import br.com.uds.udspizzaria.domain.service.TamanhoServiceInterface;
 import br.com.uds.udspizzaria.infrastructure.persistence.hibernate.repository.TamanhoRepositoryInterface;
@@ -20,6 +22,15 @@ public class TamanhoService extends BaseService<Tamanho> implements TamanhoServi
         return this.getRepository().findIdAndNomeByAll();
     }
 	
+    @Override
+    public Tamanho buscar(Long id) throws TamanhoNaoEncontradoException{
+    	try {
+        	return super.buscar(id);
+		} catch (InformacaoNaoEncontradaException exception) {
+			throw new TamanhoNaoEncontradoException();
+		}
+    }
+    
     @Override
     protected TamanhoRepositoryInterface getRepository() {
         return this.tamanhoRepository;
